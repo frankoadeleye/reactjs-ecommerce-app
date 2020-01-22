a6c26c6b7280e2aff0479ee4ad279ab080931d37
Francocean <francophoneadeleye@gmail.com>
2020-01-21 17:09:15 -0800

    checking out if it responds to changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,7 +18,7 @@ export default class Navbar extends Component{
                 <ul className="navbar-nav align-items-center">
                     <li className="nav-items ml-5">
                         <Link to="/reactjs-ecommerce-app" className="nav-link">
-                            products
+                            produ
                         </Link>
                     </li> 
                 </ul>
